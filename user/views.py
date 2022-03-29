7165c70f9bffaa5cd35c9bd4706231457c34656d
Your Name <you@example.com>
2022-03-29 17:00:25 +0800

    index

--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+import pandas
+import numpy
+import array
 # Create your views here.
+def index(request):
+    path = "E:/BRYNPRYL/user.dat"
+    # For attendance
+    # for i in open(path).readlines():
+    #     print(i.strip().split()[0])
+    
+    # For user data
+    test = int(9999999999999)
+    print(test)
+    with open(path) as datFile:
+        for data in datFile:
+            print(data.split()[0])
+            print([int(s) for s in data.split()[0] if s.isdigit()])
+
+    return render(request, 'user/index.html',{})
